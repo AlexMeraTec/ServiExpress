8a0ec2cad9e38c98b5f2dae4932a9d4e30e2d5c3
solidusmatamala <50125357+solidusmatamala@users.noreply.github.com>
2019-06-30 20:48:40 -0400

    no se
    ok

---
+++ Frontend/Probando/Probando/Controllers/TestController.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using Probando.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTutorial.Controllers
+{
+    public class TestController : Controller
+    {
+        HttpClient client;
+        private List<LoginModel> loginModels;        
+        public List<LoginModel> AllLogin
+        {
+            get { return this.loginModels; }
+        }        
+        private const string url = global.ip;
+        public TestController()
+        {
+            this.loginModels = null;            
+            client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        public ActionResult Login()
+        {
+
+            return View(new LoginModel());
+        }
+
+        [HttpPost]
+        //public JsonResult LoginUser(RegistrationViewModel model)
+        //{            
+
+        //    SiteUser user = db.SiteUsers.SingleOrDefault(x => x.EmailId == model.EmailId && x.Password == model.Password);
+        //    string result = "fail";
+        //    if (user != null)
+        //    {
+
+        //        Session["UserId"] = user.UserId;
+        //        Session["UserName"] = user.UserName;
+        //        if (user.RoleId == 3)
+        //        {
+        //            result = "GeneralUser";
+
+        //        }
+        //        else if (user.RoleId == 1)
+        //        {
+        //            result = "Admin";
+
+        //        }
+
+        //    }
+        //    return Json(result, JsonRequestBehavior.AllowGet);
+        //}
+        public async Task<ActionResult> Login(LoginModel cli)
+        {
+            var cliSon = JsonConvert.SerializeObject(cli);
+            HttpContent httpContent = new StringContent(cliSon, Encoding.UTF8, "application/json");
+            HttpResponseMessage responseMessage = await client.PostAsync("api/persona", httpContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Error");
+        }
+
+
+        public ActionResult Logout()
+        {
+
+            Session.Clear();
+            Session.Abandon();
+
+            return RedirectToAction("Login");
+
+        }
+
+
+    }
+}
